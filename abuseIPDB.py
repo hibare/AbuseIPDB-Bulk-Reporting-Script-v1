745d7517c99315d3e1390a5779eb587e04a0ff47
Shubham Hibare <shubham@hibare.in>
2018-07-27 11:38:25 +0530

    Major fix
    Removes duplicate IPs from input.

--- abuseIPDB.py
+++ abuseIPDB.py
@@ -3,11 +3,11 @@
 """ 
 	Python script to report bad IPs to AbuseIPDB in bulk
 """
+
 __author__ = "Shubham Hibare"
-__version__ = "1.2"
+__version__ = "1.3"
 __maintainer__ = "Shubham Hibare"
 __email__ = "shubham@hibare.in"
-__status__ = "Production"
 
 import requests
 import json
@@ -19,6 +19,11 @@ import ipaddress
 
 # function to validate an IP address
 def validateIP(ipaddressToValidate):
+	"""
+		Description: validates an IP address
+		Input: IP to be validated (string)
+		Return: True or False (boolean)
+	"""
 	try:
 		ipaddress.ip_address(ipaddressToValidate)
 		return True
@@ -27,56 +32,73 @@ def validateIP(ipaddressToValidate):
 
 # main function to report IPs to abuseIPDB
 def postIP(APIKey, inputFileName, category, comment):
+	"""
+		Description: post IPs to abuseIPDB
+		Input: APIKey 		 - abuseIPDB API key (string)
+			   inputFileName - input file containing IPs, one per line (string)
+			   category 	 - IP submission category (string)
+			   comment 	 - comment for IP (string)
+		Return: none
+	"""
 	ipCounter = 0
 	failedIP = []
+	inputIPList = []
 	submissionStatus = ''
 	outputFileName = 'AbuseIPDB_Submission_Status.csv'
 	fields = ['IP', 'Status'] 
 
 	try:
-		#open input file
+		# open input file and read all IPs in a list
 		inputFile = open(inputFileName, 'r')
+		inputIPList = inputFile.readlines()
+		inputFile.close()
 
-		#open output file
+		# remove duplicates
+		inputIPList = set(inputIPList)
+
+		# open output file
 		outputFile = open(outputFileName, 'w')
 
-		#get a csv writer
+		# get a csv writer
 		writer = csv.DictWriter(outputFile, fieldnames=fields)
 
-		#write header to csv file
+		# write header to csv file
 		writer.writeheader()
 
-		#loop through the input file
-		for ip in inputFile.readlines():
+		# loop through the input file
+		for ip in inputIPList:
 
-			#strip new line character from the input
+			# strip new line character from the input
 			ip = ip.rstrip('\n')
 
-			#if not a blank line
+			# if not a blank line
 			if ip:
 
-				print("[{0}] Processing IP : {1}".format(ipCounter+1, ip), end=' ... ')
+				# increment IP counter
+				ipCounter += 1
+
+				print("[{0}] Processing IP: {1}".format(ipCounter, ip), end=' ... ')
 
 				if validateIP(ip):
-					
-					#make request to geoplugin
+					# make request to geoplugin
 					payload = {'key': APIKey, 'category': category, 'comment': comment, 'ip': ip}
 					url = 'https://www.abuseipdb.com/report/json'
 
-					# make request
+					# make request to abuseIPDB API
 					abuseipdbRequest = requests.post(url, params=payload)
 					
-					# parse JSON data
-					datastore = json.loads(abuseipdbRequest.text)
-
-					#increment IP counter
-					ipCounter = ipCounter + 1
-
-					# check submission status
-					if datastore['success']:
-						submissionStatus = 'Success'
+					if abuseipdbRequest.ok:
+						# parse JSON data
+						datastore = json.loads(abuseipdbRequest.text)
+						
+						# check submission status
+						if datastore.get('success'):
+							submissionStatus = 'Success'
+						else:
+							submissionStatus = 'Failed'
+							failedIP.append(ip)
 					else:
-						submissionStatus = 'Failed'
+						submissionStatus = 'Request failed'
 						failedIP.append(ip)
 
 				else:
@@ -85,11 +107,11 @@ def postIP(APIKey, inputFileName, category, comment):
 
 				# write output
 				print("[{0}]".format(submissionStatus))
-				writer.writerow({'IP':ip, 'Status': submissionStatus})
+				writer.writerow({'IP': ip, 'Status': submissionStatus})
 
 
-		#print total number of IPs processed
-		print("\nTotal IPs processed : {0}".format(ipCounter))
+		# print total number of IPs processed
+		print("\nTotal IPs processed: {0}".format(ipCounter))
 
 		# check for failed IPs and print them
 		if len(failedIP) > 0:
@@ -99,14 +121,11 @@ def postIP(APIKey, inputFileName, category, comment):
 
 	except Exception as e:
 		print("Exception : "+str(e))
-		print("\nNumber of processed IPs : {0}".format(ipCounter))
-		print("Last processing IP : {0}".format(ip))
+		print("\nNumber of processed IPs: {0}".format(ipCounter))
+		print("Last processing IP: {0}".format(ip))
 
 	finally:
-		#Close input file
-		inputFile.close()
-
-		#close output file
+		# close output file
 		outputFile.close()
 
 
@@ -116,32 +135,31 @@ if __name__ == '__main__':
 	parser.add_argument('-f', '--inputfilename', type=str, metavar='<input filename>', help='Input file name', required=True)
 	args = parser.parse_args()
 	
-	# default grant 'no'
-	proceed = 'n
-	
 	# check if file exists and readable
 	if os.path.isfile(args.inputfilename) and os.access(args.inputfilename, os.R_OK):
-		reportingCategories = {3: 'Fraud Orders', 
-								4: 'DDoS Attack', 
-								5: 'FTP Brute-Force', 
-								6: 'Ping of Death', 
-								7: 'Phishing', 
-								8: 'Fraud VoIP', 
-								9: 'Open Proxy', 
-								10: 'Web Spam', 
-								11: 'Email Spam', 
-								12: 'Blog Spam', 
-								13: 'VPN IP', 
-								14: 'Port Scan', 
-								15: 'Hacking', 
-								16: 'SQL Injection', 
-								17: 'Spoofing', 
-								18: 'Brute-Force', 
-								19: 'Bad Web Bot', 
-								20: 'Exploited Host', 
-								21: 'Web App Attack', 
-								22: 'SSH', 
-								23: 'IoT Targeted'}
+		reportingCategories = {
+						3: 'Fraud Orders', 
+						4: 'DDoS Attack', 
+						5: 'FTP Brute-Force', 
+						6: 'Ping of Death', 
+						7: 'Phishing', 
+						8: 'Fraud VoIP', 
+						9: 'Open Proxy', 
+						10: 'Web Spam', 
+						11: 'Email Spam', 
+						12: 'Blog Spam', 
+						13: 'VPN IP', 
+						14: 'Port Scan', 
+						15: 'Hacking', 
+						16: 'SQL Injection', 
+						17: 'Spoofing', 
+						18: 'Brute-Force', 
+						19: 'Bad Web Bot', 
+						20: 'Exploited Host', 
+						21: 'Web App Attack', 
+						22: 'SSH', 
+						23: 'IoT Targeted'
+					}
 		
 		# read category
 		for key, value in reportingCategories.items():
@@ -155,20 +173,20 @@ if __name__ == '__main__':
 			if cat.isdigit() and int(cat) >= 3 and int(cat)<=23:
 				pass
 			else:
-				print("Invalid category : {0}".format(cat))
+				print("Invalid category: {0}".format(cat))
 				sys.exit()
 
 
 		# read comment
-		comment = input('\nEnter comment : ')
+		comment = input('\nEnter comment: ')
 
 		print("\nPlease verify:")
 		print("Category: {0}".format(category))
 		print("comment: {0}".format(comment))
-		proceed = input("\nContinue [y/n]? ")
+		proced = input("\nContinue [y/n]? ")
 
-		if proceed == "y":
-			#call getIPDetails function
+		if proced == "y":
+			# call getIPDetails function
 			postIP(args.APIKey, args.inputfilename, category, comment)
 		else:
 			print("abort")
